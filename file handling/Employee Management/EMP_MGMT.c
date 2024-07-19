c9d8b4348ec5775510efb9c50dc9f6bf92f7a4f3
KrishnaYamphu <krishnayamphu@gmail.com>
2024-07-19 12:49:29 +0545

    crud

--- file handling/Employee Management/EMP_MGMT.c
+++ file handling/Employee Management/EMP_MGMT.c
@@ -11,6 +11,7 @@ float salary;
 void menu();
 void showEmployee();
 void addEmployee();
+void editEmployee();
 void deleteEmployee();
 int main(){
     int choice;
@@ -25,6 +26,10 @@ int main(){
             break;
         case 2:
             addEmployee();
+            getch();
+            break;
+        case 3:
+            editEmployee();
             getch();
             break;
         case 4:
@@ -94,12 +99,63 @@ void addEmployee(){
     fclose(f);
 }
 
+
+void editEmployee(){
+    Employee e;
+    char name[20];
+    char confirm='N';
+    int count=0;
+    fp=fopen("temp.txt","w");
+    f=fopen("emp.txt","r");
+    if(f==NULL){
+        printf("File not found!");
+    }else{
+        printf("Enter update name:");
+        scanf("%s",&name);
+        while(fread(&e,sizeof(e),1,f)){
+            if(strcmpi(name,e.name)==0){
+                count=1;
+                printf("\n:::::::::::::::: Existing Details :::::::::::::::::\n\n");
+                printf(" Name \t\t\t\t\tSalary\n");
+                printf("---------------------------------------------------\n");
+                printf(" %-39s%0.2f\n",e.name,e.salary);
+                printf("---------------------------------------------------\n");
+                printf("\n\nAre you sure update this record? (Y/N):");
+                confirm=getche();
+                if(confirm=='Y'||confirm=='y'){
+                        printf("\nEnter new name:");
+                        scanf("%s",&e.name);
+                        printf("Enter new salary:");
+                        scanf("%f",&e.salary);
+                        fwrite(&e,sizeof(e),1,fp);
+                        printf("Data updated!");
+                }else{
+                    fwrite(&e,sizeof(e),1,fp);
+                }
+            }else{
+                fwrite(&e,sizeof(e),1,fp);
+            }
+
+        }
+        printf("count: %d",count);
+        if(count==0){
+            printf("\nEmployee data not found!\n");
+        }
+        fclose(f);
+        fclose(fp);
+        remove("emp.txt");
+       rename("temp.txt","emp.txt");
+    }
+
+
+}
+
+
 void deleteEmployee(){
     Employee e;
     char name[20];
     char confirm='N';
     int count=0;
-    int status;
     fp=fopen("temp.txt","w");
     f=fopen("emp.txt","r");
     if(f==NULL){
@@ -115,37 +171,28 @@ void deleteEmployee(){
                 printf("---------------------------------------------------\n");
                 printf(" %-39s%0.2f\n",e.name,e.salary);
                 printf("---------------------------------------------------\n");
+                printf("\n\nAre you sure delete this record? (Y/N):");
+                confirm=getche();
+                if(confirm=='Y'||confirm=='y'){
+                        printf("\nData Deleted Successfully.");
+                    continue;
+                }else{
+                fwrite(&e,sizeof(e),1,fp);
+                }
             }
+            fwrite(&e,sizeof(e),1,fp);
         }
-        rewind(f);
         printf("count: %d",count);
         if(count==0){
             printf("\nEmployee data not found!\n");
-        }else{
-            while(fread(&e,sizeof(e),1,f)){
-                if(strcmpi(name,e.name)==0){
-                    printf("\n\nAre you sure delete this record? (Y/N):");
-                    confirm=getche();
-                    if(confirm=='Y'||confirm=='y'){
-                            printf("\nData Deleted Successfully.");
-                        continue;
-                    }else{
-                    fwrite(&e,sizeof(e),1,fp);
-                    }
-                }
-                fwrite(&e,sizeof(e),1,fp);
-            }
-
         }
         fclose(f);
         fclose(fp);
         remove("emp.txt");
-        status=rename("temp.txt","emp.txt");
+       rename("temp.txt","emp.txt");
     }
 
-    if(status==0){
-        //printf("Data Deleted Successfully.");
-    }
+
 }
 
 
